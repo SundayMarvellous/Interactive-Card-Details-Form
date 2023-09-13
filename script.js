ba9f4d3c4543771a4bdede909b2c9ae8247b6ed0
SundayMarvellous <114695884+SundayMarvellous@users.noreply.github.com>
2023-09-13 12:24:17 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// function myfunction(e) { 
+//     // e.preventDefault();
+//     var myText = document.getElementById("cardholder-name").value;
+//     document.getElementById('cardholdername').innerHTML = myText;
+//     var myNumber = document.getElementById("card-number").value;
+//     document.getElementById('cardholdernumber').innerHTML = myNumber;
+//     var myCvc = document.getElementById("cvc").value;
+//     document.getElementById('cardcvc').innerHTML = myCvc;
+//     var myMonth = document.getElementById("card-month").value;
+//     document.getElementById('cardmonth').innerHTML = myMonth;
+//     var myYear = document.getElementById("card-year").value;
+//     document.getElementById('cardyear').innerHTML = myYear;
+//  } 
